34acdc996dfd16ca1d2ae4bf60072532bff18e93
Coder-Deepanshu <d25611372@gmail.com>
2026-07-15 23:56:38 +0530

    sring methods notes

--- notes01.js
+++ notes01.js
@@ -4,8 +4,8 @@ var name2 = "Yashika"; // variable global scope
 const name3 = "Manu"; // constants are declared once, and used many times.
 name4 = "Mayank"; // This is another to declare variables in javascript, but it is not good.
 
-console.log(name1); // by this we can see the output of single variable.
-console.table([name1, name2, name3, name4]); // by this we can see output of many variables in table
+// console.log(name1); // by this we can see the output of single variable.
+// console.table([name1, name2, name3, name4]); // by this we can see output of many variables in table
 
 // data types
 let name = "Deepanshu"; // string
@@ -13,29 +13,25 @@ let age = 18; // number
 let adult = true; // boolean (true/false)
 let location; // it give undefined / it is a undefined data type
 let address = null; // it assign null to variable / it is a object data type
-console.table([typeof name, typeof age, typeof adult, typeof location, typeof address]) // first way of getting data type of any variable or value
-console.log(typeof(adult)) // also as a method we can use typeof 
+// console.table([typeof name, typeof age, typeof adult, typeof location, typeof address]) // first way of getting data type of any variable or value
+// console.log(typeof(adult)) // also as a method we can use typeof 
 
 // conversion of data types
 // we convert one data type into another by using that functions:
-console.log(Number(name)); // it give Nan which means (not a number)
-console.log(Boolean(location)); // it is use for converting in true or false value
-console.log(String(age)) // it is use to converting any value into string 
+// console.log(Number(name)); // it give Nan which means (not a number)
+// console.log(Boolean(location)); // it is use for converting in true or false value
+// console.log(String(age)) // it is use to converting any value into string 
 
 // string to number confusing conversion
-console.log(1 + 1 + "2");
-console.log("1" + 1 + 2);
+// console.log(1 + 1 + "2");
+// console.log("1" + 1 + 2);
 
 // prefix and postfix plus and minus operator
 let x = 5;
 let y = ++x; // firstly variable value changed and then used , phle x 6 ban gya fr y 6 ban gaya
-console.table([x , y]);
+// console.table([x , y]);
 
 let w = 5
 let z = w++; // firstly variable value used and then changed, phle w ki value use hui fr w ki value change ho gai
-console.table([w, z])
+// console.table([w, z])
 
-// JAVA SCRIPT DATA TYPES:
-// 1. Strings
-const name = "Deepanshu"; // String Declaration
-console.log(name);
---
+++ notes02.js
@@ -0,0 +1,78 @@
+// JAVA SCRIPT DATA TYPES:
+// 1. Strings
+const firstName = "Deepanshu"; // String Declaration
+// console.log(firstName);
+
+// Strings Methods
+// 1. includes() : ye check krta ha kya ak string dusri string ke andur ha ya nhi (ye case sensitive hota ha )
+// outcome come in true or false
+let text = "Hello Bhaiya"
+// console.log(text.includes("Bhaiya"));
+// console.log(text.includes("bhaiya"));
+
+// 2. indexOf():
+// a. ye 0 based indexing follow krta ha 
+// b. ye kisi character ya substring ka pehla index (position) batata hai jahan wo start hota ha
+// c. ager ise wo string na mile to ye -1 return kr deta ha 
+let anotherText = "java Script";
+// console.log(anotherText.indexOf("Script"));
+// console.log(anotherText.indexOf("Python"));
+// console.log(anotherText.indexOf("script"));
+// console.log(anotherText.indexOf("Scripts"));
+
+// 3.startWith() / endWith():
+// a. startWith(): check karta ha ki koi string kisi khas letter se ya character se start ho rhi ha ya nhi 
+// b. endWith(): check karta ha ki koi string kisi khas letter ya character se end ho rhi ha ya nhi 
+
+// lets take the example
+// console.log(anotherText.startsWith("j"));
+// console.log(anotherText.endsWith("t"));
+
+// 4. toLowerCase() / toUpperCase:
+// a. toLowerCase(): puri string ko small letters me convert kar deta ha 
+// b. toUpperCase(): puri string ko big letters me convert kar deta ha 
+const anotherName = "Amit Kumar is is is is ";
+// console.log(anotherName.toLowerCase());
+// console.log(anotherName.toUpperCase());
+
+// 5. trim(): string ke starting aur ending ke be faltu ke spaces ko hta deta ha , bich ke space ko touch bhi nhi karta
+const demo = "            Hello World          "
+// console.log(demo.trim());
+
+// 6. replaceAll()/ replace()
+// a. replace(): kisi word ya character ki pehli occurence ko badal deta ha 
+// b. replaceAll(): String mein jahan-jahan wo character ya letter hoga , sabko badal dena
+// console.log(anotherName.replace("Amit", "Rajesh"));
+// console.log(anotherName.replaceAll("is", "was"));
+
+// 7. slice(): String me se ak tukda kat ke dena jise hum sub-string khte ha , isme hum start aur end index dete ha 
+// Note: end wala index exclude hota ha 
+console.log(anotherName.slice(0,-5))
+
+// 8. split(): String ko kisi comma space etc ke basis par todta ha hor sub strings ka array create kar deta ha 
+let fruits = "Apple, Banana, Mango, Litchi";
+console.log(fruits.split(","));
+
+// 9. substring(): ye bhi slice ki trh kam karta ha , but faraq ye ha slice negative values accept kar letaha , ye negative values ko 0 treat karta ha 
+console.log(anotherName.substring(0,8));
+
+// 10. charAt(): kisi specific index par kaun sa character baitha hai ye batata ha 
+console.log(anotherName.charAt(0)); 
+
+// 11. length: ye ak property ha jo string ki length btati ha means string me kitne characters ha including spaces
+console.log(anotherName.length)
+
+// 12. repeat(): ek string ko jitni bar chaho utni bar repeat krke new string bna skte ho
+console.log(anotherText.repeat(3));
+
+// 13. match(): ye string ko ek regular expression ke sath match krne ke liye use hota ha ayr matches array ko return karta ha 
+let demoText = "My lucky number are 7 and 9";
+let regx = /[0-9]/g; // sare numbers find krne ke liye regex
+console.log(demoText.match(regx));
+
+// 14. padStart() / padEnd():
+// a. padStart(): string ke start me tab tak character add krta rehta ha jab tk string di gai length na ho jaye
+// b. padEnd(): ye string ke end me add krta ha 
+let cardNo = "4512"
+console.log(cardNo.padStart(8,"*"))
+console.log(cardNo.padEnd(8, "-"));
