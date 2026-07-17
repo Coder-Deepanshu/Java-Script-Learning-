3b0fb174ed25c1397cd3572eef17c1305509a6cb
Coder-Deepanshu <d25611372@gmail.com>
2026-07-17 23:13:25 +0530

    today learning the few methods regarding date and time

--- notes05.js
+++ notes05.js
@@ -3,3 +3,34 @@
 // 1. Create the Date Object
 let now = new Date();
 console.log(now);
+
+// 2. For getting the information from the data
+// Methods of Date 
+
+// a. getFullYear(): give the year
+console.log(now.getFullYear())
+
+// b. getMonth(): give the month number by 0-11 based numering
+console.log(now.getMonth())
+
+// c. getDate(): give the date of the data
+console.log(now.getDate())
+
+// d. getDay(): give the day number from 1 to 7
+console.log(now.getDay())
+
+
+// Methods for Time
+
+// a. getHours(), getMinutes(), getSeconds(), for getting minutes , hours , seconds
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+
+// b. getTime(): ye hume 1 January 1970 se lekar ajj tk ka exact timestamp de deta ha  in miliseconds
+console.log(now.getTime())
+
+// Methods for setting the Date and Time 
+
+// a. setFullYear(): ye year set kr deta ha
+console.log(now.setFullYear(2020))
